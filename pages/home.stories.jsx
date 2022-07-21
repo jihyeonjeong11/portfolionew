1b2c3ad5828b01f7ee656bc32b4b6c1a2eb51cfb
jihyeonjeong11 <wjdwlgus11@gmail.com>
2022-07-21 09:47:57 +0900

    initial commit

---
+++ pages/home.stories.jsx
@@ -0,0 +1,12 @@
+// /stories/pages/home.stories.jsx
+
+import Home from "../../pages/index";
+import "../styles/globals.css";
+
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+export const HomePage = () => <Home />
