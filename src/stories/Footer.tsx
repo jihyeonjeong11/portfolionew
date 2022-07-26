064ea65d13d4909ae61d75ebc10cca692dab25b0
jihyeonjeong11 <wjdwlgus11@gmail.com>
2022-07-26 11:18:36 +0900

    footer fix

--- src/stories/Footer.tsx
+++ src/stories/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
                 <h1 className="text-2xl text-bold">Contact.</h1>
                 <div className="mt-5">
-                    <Socials />
+                    <Socials className={''} />
                 </div>
             </div>
             {/* <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
